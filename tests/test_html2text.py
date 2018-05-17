9475e8a7eef768cbe4b6898d4b8a326628cb07f7
Carson Ip <carsonip715@gmail.com>
2018-05-17 17:08:06 +0800

    Add tests

---
+++ tests/test_html2text.py
@@ -0,0 +1,21 @@
+import pytest
+from pyhtml2text import html2text
+
+
+def test_simple_html():
+    html = '<body>foo</body>'
+    assert html2text(html) == 'foo\n'
+
+
+def test_large_html():
+    html = '<body><div>%s</div></body>' % ('a' * 100000)
+    assert html2text(html) == ('a' * 100000) + '\n'
+
+
+def test_br_in_div():
+    # There should be only 1 \n instead of 2 unless there is something after <br>
+    html = '<body>foo<div><br></div>bar</body>'
+    assert html2text(html) == 'foo\nbar'
+
+    html = '<body>foo<div><br>bar</div>baz</body>'
+    assert html2text(html) == 'foo\nbar\nbaz'
