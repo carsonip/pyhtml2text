e41c6a9ce66af2507816561437d02da19380bdd9
Carson Ip <carsonip715@gmail.com>
2018-05-17 19:08:00 +0800

    Fix tests for python3

--- tests/test_html2text.py
+++ tests/test_html2text.py
@@ -3,19 +3,19 @@ from pyhtml2text import html2text
 
 
 def test_simple_html():
-    html = '<body>foo</body>'
-    assert html2text(html) == 'foo\n'
+    html = b'<body>foo</body>'
+    assert html2text(html) == b'foo\n'
 
 
 def test_large_html():
-    html = '<body><div>%s</div></body>' % ('a' * 100000)
-    assert html2text(html) == ('a' * 100000) + '\n'
+    html = b'<body><div>%s</div></body>' % (b'a' * 100000)
+    assert html2text(html) == (b'a' * 100000) + b'\n'
 
 
 def test_br_in_div():
     # There should be only 2 \n instead of 3 unless there is something after <br>
-    html = '<body>foo<div><br></div>bar</body>'
-    assert html2text(html) == 'foo\n\nbar\n'
+    html = b'<body>foo<div><br></div>bar</body>'
+    assert html2text(html) == b'foo\n\nbar\n'
 
-    html = '<body>foo<div><br>bar</div>baz</body>'
-    assert html2text(html) == 'foo\n\nbar\nbaz\n'
+    html = b'<body>foo<div><br>bar</div>baz</body>'
+    assert html2text(html) == b'foo\n\nbar\nbaz\n'
